1c958c8f085c82381c37259dadf4d08d7f8caf48
rednayan <nayansharma1010@gmail.com>
2023-07-13 12:33:21 +0530

    feat: drawing circles and rects

--- main.c
+++ main.c
@@ -2,10 +2,19 @@
 
 int main(void) {
   InitWindow(800, 600, "Pong");
+  SetWindowState(FLAG_VSYNC_HINT);
+
+  float ball_x = GetScreenWidth() / 2.0;
+  float ball_y = GetScreenHeight() / 2.0;
+  float ball_radius = 5.0;
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(WHITE);
+    DrawCircle(ball_x, ball_y, ball_radius, BLACK);
+    DrawRectangle(50, GetScreenHeight() / 2 - 50, 10, 100, BLACK);
+    DrawRectangle(GetScreenWidth() - 50 - 10, GetScreenHeight() / 2 - 50, 10,
+                  100, BLACK);
     DrawFPS(10, 10);
     EndDrawing();
   }
